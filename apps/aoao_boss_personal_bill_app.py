c431d4d2e34a58de9ee836c9665ad7a56f8780b2
pql <1223750634@qq.com>
2019-06-14 11:34:36 +0800

    update multi app

--- apps/aoao_boss_personal_bill_app.py
+++ apps/aoao_boss_personal_bill_app.py
@@ -2,37 +2,36 @@
 
 from datetime import datetime
 
-import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
+
+from app import app
 
 
 from pymongo import MongoClient
 conn = MongoClient("mongodb://127.0.0.1")
 db = conn['boss-dev']
 
-external_stylesheets = [
-    'https://codepen.io/chriddyp/pen/bWLwgP.css',
-    {
-        'href': 'https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css',
-        'rel': 'stylesheet',
-    }
-]
 
-app = dash.Dash(
-    __name__,
-    # requests_pathname_prefix='/boss',
-    external_stylesheets=external_stylesheets
-)
+def find_identity_card_ids():
+    # identity_card_ids = db.elem_knight_daily_data.group([
+    #     {"$group": {"_id" : "$identity_card_id"}},
+    #     {'$limit': 10}
+    # ])
+
+    daily_data = db.elem_knight_daily_data.find({}).limit(100)
+    identity_card_ids = [data['identity_card_id'] for data in daily_data]
+    return list(set(identity_card_ids))
 
 
-def find_personal_data(identity_card_id):
+def build_personal_figure_data(id_card, show_type='', min_day=20171201, max_day=20171231):
     x_data = []
     y_data_order_quantity = []
-    min_day = 20171201
-    max_day = 20171231
+    y_data_finish_order_quantity = []
+    y_data_cancel_order_quantity = []
     spec = {
-        'identity_card_id': identity_card_id,
+        'identity_card_id': id_card,
         'date': {'$gte': min_day, '$lte': max_day},
     }
     daily_data = db.elem_knight_daily_data.find(spec).sort([('date', 1)])
@@ -41,38 +40,64 @@ def find_personal_data(identity_card_id):
         day = current_date.day
         x_data.append(day)
         y_data_order_quantity.append(data['order_quantity'])
-    return x_data, y_data_order_quantity
+        y_data_finish_order_quantity.append(data['finish_order_quantity'])
+        y_data_cancel_order_quantity.append(data['cancel_order_quantity'])
+
+    return {
+        'data': [
+            {
+                'x': x_data,
+                'y': y_data_order_quantity,
+                'text': [],
+                'name': u'总单数',
+                'type': show_type
+            },
+            {
+                'x': x_data,
+                'y': y_data_finish_order_quantity,
+                'text': [],
+                'name': u'完成单数',
+                'type': show_type,
+            },
+            {
+                'x': x_data,
+                'y': y_data_cancel_order_quantity,
+                'text': [],
+                'name': u'超时单数',
+                'type': show_type,
+            }
+        ]
+    }
 
 layout = html.Div(children=[
     html.Div(
         children=[
             dcc.Dropdown(
-                id='crossfilter-xaxis-column',
-                options=[{'label': i, 'value': i} for i in [1, 2, 3]],
-                value='Fertility rate, total (births per woman)'
+                id='dp-id-card',
+                options=[{'label': identity_card_id, 'value': identity_card_id} for identity_card_id in find_identity_card_ids()],
+                value=''
             ),
             # 折线图
             dcc.Graph(
-                id='graph-bill-scatter',
-                figure={
-                    'data': [
-                        {
-                            'x': find_personal_data('452427199610223913')[0],
-                            'y': find_personal_data('452427199610223913')[1],
-                            'text': [],
-                            'name': u'总单数',
-                            'type': 'scatter',
-                        }
-                    ],
-                    'layout': {
-                        'clickmode': 'event+select'
-                    }
-                }
+                id='graph-personal-bill-scatter'
             ),
-
             # 直方图
             dcc.Graph(
-                id='graph-bill-bar',
+                id='graph-personal-bill-bar',
             ),
         ]),
 ])
+
+
+@app.callback(
+    [Output('graph-personal-bill-scatter', 'figure'),
+     Output('graph-personal-bill-bar', 'figure')],
+    [Input('dp-id-card', 'value')])
+def update_personal_figure(id_card):
+    """
+
+    :param id_card:
+    :return:
+    """
+
+    return build_personal_figure_data(id_card, 'scatter'), build_personal_figure_data(id_card, 'bar')
